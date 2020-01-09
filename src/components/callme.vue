9031dbef70529485e05f7ba692c068e608f5031e
macheng2017 <gos360@163.com>
2020-01-09 22:08:17 +0800

    add:$on $emit

--- src/components/callme.vue
+++ src/components/callme.vue
@@ -16,9 +16,14 @@
         created() {
             // 先注册一个事件,等待被emit
             this.$on('my_event', this.handleEvent)
+            this.$on('my_event', this.handleEvent2)
         },
         methods: {
             handleEvent(e) {
+                // eslint-disable-next-line no-console
+                console.log(this.message, e)
+            },
+            handleEvent2(e) {
                 // eslint-disable-next-line no-console
                 console.log(this.message, e)
             },
